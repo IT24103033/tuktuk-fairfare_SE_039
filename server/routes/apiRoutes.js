6dd2e149518c9fc47381b84770c14c1e1e693f25
sanuthmibandara <sanuthmibandara@gmail.com>
2026-09-04 12:06:24 +0530

    Added AI fare assessment endpoint using Gemini 3.6 Flash

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { calculateFare, getBenchmarks } = require('../controllers/fareController');
+const { calculateFare, getBenchmarks, assessFareAI } = require('../controllers/fareController');
 
 // POST request because the UI will send distance/time data to calculate
 router.post('/calculate', calculateFare);
+router.post('/assess', assessFareAI);
 
 // GET request because the UI just needs to fetch the route benchmark list
 router.get('/benchmarks', getBenchmarks);
